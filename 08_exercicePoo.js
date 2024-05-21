fec318a88c895f0a53d7607bda362060ff80c4dd
klabarre <klabarre@dawan.fr>
2024-05-21 13:43:08 +0200

    Correction exercice

--- 08_exercicePoo.js
+++ 08_exercicePoo.js
@@ -19,3 +19,65 @@
 
 //     Redéfinissez la méthode calculateSalary() dans la classe Manager pour prendre en compte le bonus de performance en fonction du pourcentage de bonus.
 //       ==> calculeSalary() renverra -> baseSalary * bonusPercentage;
+
+// Partie 1
+
+class Employee {
+    constructor(name, age, baseSalary, employeeId) {
+        this.name = name;
+        this.age = age;
+        this.baseSalary = baseSalary;
+        this.employeeId = employeeId;
+    }
+
+    calculateSalary() {
+        return this.baseSalary * 1.1
+    }
+}
+
+// PARTIE 2
+
+// Je n'ai pas besoin de constructeurs. Une classe enfant sans constructeur utilisera
+// le constructeur de la classe parente.
+class Manager extends Employee {
+    #bonusPercentage = 1.15;
+
+    calculateSalary() {
+        return this.baseSalary * this.#bonusPercentage
+    }
+}
+
+
+employe = new Employee('Jean', 42, 1000);
+manager = new Manager('Jean', 42, 1000);
+
+console.log(employe.calculateSalary())
+console.log(manager.calculateSalary())
+
+// __________________________________________________________________________________________________________________
+
+// Exemple avec getter / setter (hors consigne)
+class Manager2 extends Employee {
+    #bonusPercentage = 1.15;
+
+    calculateSalary() {
+        return this.baseSalary * this.#bonusPercentage
+    }
+
+    get bonusPercentage (){
+        return this.#bonusPercentage
+    }
+
+    set bonusPercentage(newBonusPercentage) {
+        this.#bonusPercentage = newBonusPercentage
+    }
+
+}
+
+
+manager2 = new Manager2('Jean', 42, 1000);
+
+console.log(manager2.bonusPercentage)
+
+manager2.bonusPercentage = 5.3
+console.log(manager2.bonusPercentage)
